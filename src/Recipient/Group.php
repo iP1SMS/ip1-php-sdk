44915ba6cca2e169284d5c1b63f11d8ce28d9f53
Hannes Kindströmmer <hannes@kindstrommer.se>
2017-03-15 10:51:22 +0100

    All file DocBlock are the same

--- src/Recipient/Group.php
+++ src/Recipient/Group.php
@@ -1,11 +1,13 @@
 <?php
 /**
-* Contains the Group class
 * PHP version 7.1.1
 * @author Hannes Kindströmmer <hannes@kindstrommer.se>
 * @copyright 2017 IP1 SMS
+* @license https://www.gnu.org/licenses/lgpl-3.0.txt LGPL-3.0
 * @version 0.1.0-beta
-* @package IP1\RESTClient\Recipient
+* @since File available since Release 0.1.0-beta
+* @link http://api.ip1sms.com/Help
+* @link https://github.com/iP1SMS/ip1-php-sdk
 */
 namespace IP1\RESTClient\Recipient;
 
--- tests/sms/OutGoingSMSTest.php
+++ tests/sms/OutGoingSMSTest.php
@@ -1,4 +1,14 @@
 <?php
+/**
+* PHP version 7.1.1
+* @author Hannes Kindströmmer <hannes@kindstrommer.se>
+* @copyright 2017 IP1 SMS
+* @license https://www.gnu.org/licenses/lgpl-3.0.txt LGPL-3.0
+* @version 0.1.0-beta
+* @since File available since Release 0.1.0-beta
+* @link http://api.ip1sms.com/Help
+* @link https://github.com/iP1SMS/ip1-php-sdk
+*/
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Exception;
 use IP1\RESTClient\SMS\OutGoingSMS;
